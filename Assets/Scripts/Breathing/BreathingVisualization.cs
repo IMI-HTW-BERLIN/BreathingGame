c04776ca9c3fa9b9b8a9b0db6a0a6c13b99b531a
DavidPgl <panagiotopulos.d@gmail.com>
2020-09-29 20:38:10 +0200

    [Fixed BreathingVisualization]

---
+++ Assets/Scripts/Breathing/BreathingVisualization.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Managers;
+using UnityEngine;
+
+namespace Breathing
+{
+    public class BreathingVisualization : MonoBehaviour
+    {
+        [SerializeField] private string arduinoPort;
+
+        private void Start()
+        {
+            BreathingManager.Instance.StartArduinoReading(arduinoPort);
+        }
+    }
+}
